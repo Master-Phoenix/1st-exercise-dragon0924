f23a719e6ac6e1801e9b4e204ceae20fae238819
dragon0924 <lexus_430s@yahoo.com>
2020-05-20 14:39:11 +0700

    Update Sum.c

--- Sum.c
+++ Sum.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 int Sum(int a, int b){
    return a+b;
 }
-int main() {
-   
+
+int main(int argc, char **argv)
+{
+   int a = atoi(argv[0]);
+   int b = atoi(argv[1]);
+   printf('%d', Sum(a, b));
 }
